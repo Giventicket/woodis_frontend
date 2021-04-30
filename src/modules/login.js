df327a576ebf362e2576e77e7b9fa05a898d4915
GivenTicket <jpseo99@gmail.com>
2021-04-30 10:58:42 +0900

    [fix] variant name

--- src/modules/login.js
+++ src/modules/login.js
@@ -1,22 +1,31 @@
-import {createAction, handleActions} from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
 
-const CHANGE_ID = 'input/CHANGE_ID';
-const CHANGE_PASSWORD = 'input/CHANGE_PASSWORD';
-const RESET = 'input/RESET';
+const CHANGE_ID = "login/CHANGE_ID";
+const CHANGE_PASSWORD = "login/CHANGE_PASSWORD";
+const RESET = "login/RESET";
 
 export const change_id = createAction(CHANGE_ID, id => id);
-export const change_password = createAction(CHANGE_PASSWORD, password => password);
+export const change_password = createAction(
+  CHANGE_PASSWORD,
+  password => password
+);
 export const reset = createAction(RESET);
 
 const initialState = {
-	id:"",
-	password:""
-}
+  id: "",
+  password: "",
+};
 
-const login = handleActions({
-	[CHANGE_ID] : (state, {payload : id}) => ({...state, id}),
-	[CHANGE_PASSWORD] : (state, {payload : password}) => ({...state, password}),
-	[RESET] : (state) => ({id:"", password:""})
-}, initialState);
+const login = handleActions(
+  {
+    [CHANGE_ID]: (state, { payload: id }) => ({ ...state, id }),
+    [CHANGE_PASSWORD]: (state, { payload: password }) => ({
+      ...state,
+      password,
+    }),
+    [RESET]: state => ({ id: "", password: "" }),
+  },
+  initialState
+);
 
 export default login;
